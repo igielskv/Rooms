8ef327becddf028b406690c24ec9ff01e8eaffbb
Vladislav Igielski <igielskv@icloud.com>
2020-07-19 10:43:18 +0200

    Adds 'video' icon to RoomDetail view, also replacing Assets with high-res room images.

--- Rooms/RoomDetail.swift
+++ Rooms/RoomDetail.swift
@@ -13,18 +13,31 @@ struct RoomDetail: View {
     @State private var zoomed = false
     
     var body: some View {
-        Image(room.imageName)
-            .resizable()
-            .aspectRatio(contentMode: zoomed ? .fill : .fit)
-            .navigationBarTitle(Text(room.name), displayMode: .inline)
-            .onTapGesture {
-                withAnimation { self.zoomed.toggle() }
+        ZStack(alignment: .topLeading) {
+            Image(room.imageName)
+                .resizable()
+                .aspectRatio(contentMode: zoomed ? .fill : .fit)
+                .navigationBarTitle(Text(room.name), displayMode: .inline)
+                .onTapGesture {
+                    withAnimation(.easeInOut(duration: 2)) { self.zoomed.toggle() }
             }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            
+            if room.hasVideo && !zoomed {
+                Image(systemName: "video.fill")
+                    .font(.title)
+                    .padding(.all)
+                    .transition(.move(edge: .leading))
+            }
+        }
     }
 }
 
 struct RoomDetail_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView { RoomDetail(room: testData[0]) }
+        Group {
+            NavigationView { RoomDetail(room: testData[0]) }
+            NavigationView { RoomDetail(room: testData[1]) }
+        }
     }
 }
